b5b27bd24ee4d96393f271c6bf9d1d98906c0256
Simon Ho <simonsays87@googlemail.com>
2018-04-18 23:04:34 -0700

    Add template for PA module

---
+++ sensormotion/pa.py
@@ -0,0 +1,8 @@
+"""
+Calculate physical activity (PA) levels with conversion to activity counts.
+
+Functions for converting raw sensor data to physical activity (PA) or
+moderate-to-vigorous physical activity (MVPA) counts, similar to those given
+by dedicated accelerometers such as Actigraph devices.
+"""
+
